a30a417f860ee8f6efb08a435951c254fcfece38
ravisankarchinnam <ravisankarchinnam@gmail.com>
2025-06-27 04:23:01 +0200

    generate all modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TemplatesModule } from './templates/templates.module';
+import { ObjectsModule } from './objects/objects.module';
+import { InspectionsModule } from './inspections/inspections.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [TemplatesModule, ObjectsModule, InspectionsModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
